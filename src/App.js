05653e67039cea8d859788487e57481b67d974f8
acreKspencer <kyle.spencer@acretrader.com>
2022-01-03 11:56:59 -0600

    css test commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hero from './Hero';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hero.js
@@ -0,0 +1,258 @@
+import React, {Fragment} from 'react';
+import App from './App';
+import './App.css';
+import logo from './assets/AcreTrader_Logo.jpg';
+
+function Hero() {
+  return (
+    <div className="Hero">
+      <div className='container'>
+      <div className='topBar'>
+      <img className='logo' src={logo} />
+      <div className='btnCont'>
+      <button className='clearBtn'>Home</button>
+      <button className='clearBtn'>Properties</button>
+      <button className='clearBtn'>Agents</button>
+      <button className='clearBtn'>Blog</button>
+      <button className='logBtn'>Log In</button>
+      </div>
+      </div>
+
+      <div className='imageCont'>
+      <div className='imgTitle'>
+        <p>Some Title Could Go Here</p>
+      </div>
+
+      <div className='imgSubtitle'>
+      <p>Some Subtitle Could go here, under the title</p>
+      </div>
+
+
+      <div className='imageMenu'>
+      <div className='imageRow'>
+        <button className='imageBtn'>Invest</button>
+        <button className='imageBtn2' >Purchase</button>
+
+      </div>
+
+      <div className='imageRow2'>
+        <div className='imageCol'>
+          <p>Property Category</p>
+          <select>
+            <option>Farms</option>
+          </select>
+        </div>
+        <div className='imageCol'>
+          <p>Price Range</p>
+          <select>
+            <option>0-$200,000</option>
+          </select>
+        </div>
+        <div className='imageCol'>
+        <button className='searchBtn'></button>
+        </div>
+      </div>
+
+
+
+      </div>
+      </div>
+
+      <div className='interTitle'>
+      <p>Most Beautiful Countries On Earth</p>
+      </div>
+
+      <div className='cardRow'>
+        <div className='card1'>
+        <p className='cardTitle'>Russia</p>
+        </div>
+        <div className='card2'>
+        <p className='cardTitle'>Ukraine</p>
+        </div>
+        <div className='card3'>
+        <p className='cardTitle'>Ukraine</p>
+        </div>
+        <div className='card4'>
+        <p className='cardTitle'>Ukraine</p>
+        </div>
+      </div>
+
+      <div className='interTitle2'>
+      <p>Recently Added</p>
+      <p className='interTitlep2'>See All</p>
+      </div>
+
+      <div className='addyCont'>
+        {/*  */}
+      <div className='addyCard'>
+      <div className='addySplit'>
+      <div className='addyImg'></div>
+      </div>
+
+      <div className='addyData'>
+      <h3 className='addyTitle'>Location Address</h3>
+        <div className='addyRow'>
+          <p>Bedrooms</p>
+          <p>Bathrooms</p>
+          <p>Sq. Footage</p>
+        </div>
+        <div className='addyRow'>
+        <p>Posted By: Name</p>
+        <button className='addyBtn'>$54,000</button>
+        </div>
+        </div>
+      </div>
+      {/*  */}
+              {/*  */}
+              <div className='addyCard'>
+      <div className='addySplit'>
+      <div className='addyImg2'></div>
+      </div>
+
+      <div className='addyData'>
+      <h3 className='addyTitle'>Location Address</h3>
+        <div className='addyRow'>
+          <p>Bedrooms</p>
+          <p>Bathrooms</p>
+          <p>Sq. Footage</p>
+        </div>
+        <div className='addyRow'>
+        <p>Posted By: Name</p>
+        <button className='addyBtn'>$54,000</button>
+        </div>
+        </div>
+      </div>
+      {/*  */}
+              {/*  */}
+              <div className='addyCard'>
+      <div className='addySplit'>
+      <div className='addyImg3'></div>
+      </div>
+
+      <div className='addyData'>
+      <h3 className='addyTitle'>Location Address</h3>
+        <div className='addyRow'>
+          <p>Bedrooms</p>
+          <p>Bathrooms</p>
+          <p>Sq. Footage</p>
+        </div>
+        <div className='addyRow'>
+        <p>Posted By: Name</p>
+        <button className='addyBtn'>$54,000</button>
+        </div>
+        </div>
+      </div>
+      {/*  */}
+        {/*  */}
+        <div className='addyCard'>
+      <div className='addySplit'>
+      <div className='addyImg'></div>
+      </div>
+
+      <div className='addyData'>
+      <h3 className='addyTitle'>Location Address</h3>
+        <div className='addyRow'>
+          <p>Bedrooms</p>
+          <p>Bathrooms</p>
+          <p>Sq. Footage</p>
+        </div>
+        <div className='addyRow'>
+        <p>Posted By: Name</p>
+        <button className='addyBtn'>$54,000</button>
+        </div>
+        </div>
+      </div>
+      {/*  */}
+              {/*  */}
+              <div className='addyCard'>
+      <div className='addySplit'>
+      <div className='addyImg2'></div>
+      </div>
+
+      <div className='addyData'>
+      <h3 className='addyTitle'>Location Address</h3>
+        <div className='addyRow'>
+          <p>Bedrooms</p>
+          <p>Bathrooms</p>
+          <p>Sq. Footage</p>
+        </div>
+        <div className='addyRow'>
+        <p>Posted By: Name</p>
+        <button className='addyBtn'>$54,000</button>
+        </div>
+        </div>
+      </div>
+      {/*  */}
+              {/*  */}
+              <div className='addyCard'>
+      <div className='addySplit'>
+      <div className='addyImg3'></div>
+      </div>
+
+      <div className='addyData'>
+      <h3 className='addyTitle'>Location Address</h3>
+        <div className='addyRow'>
+          <p>Bedrooms</p>
+          <p>Bathrooms</p>
+          <p>Sq. Footage</p>
+        </div>
+        <div className='addyRow'>
+        <p>Posted By: Name</p>
+        <button className='addyBtn'>$54,000</button>
+        </div>
+        </div>
+      </div>
+      {/*  */}
+      </div>
+
+      <div className='imageCont2'>
+      <div className='imgTitle2'>
+        <p>Some Title Could Go Here</p>
+      </div>
+
+      <div className='imgSubtitle2'>
+      <p>Some Subtitle Could go here, under the title</p>
+      <button className='addyBtn'>Learn More</button>
+      </div>
+      </div>
+      {/*  */}
+      <div className='bottomBar'>
+      <div className='bottomCol'>
+      <img className='logo2' src={logo} />
+      <p>safj jsnala s lkalks lkv  lknlkaslnlsa aopvpnw vpsknq we vns awlknglknbjkal  dknalksdn  eoklalkn alsk vna ndln gbpsocb skknbielns lbknspod lkns rlnblks slks. Tneno apbnlka</p>
+      </div>
+      <div className='bottomCol'>
+        <label>Pages</label>
+      <button className='clearBtn2'>Home</button>
+      <button className='clearBtn2'>Properties</button>
+      <button className='clearBtn2'>Agents</button>
+      <button className='clearBtn2'>Blog</button>
+      <button className='clearBtn2'>Log In</button>
+      </div>
+      <div className='bottomCol'>
+      <label>Learn</label>
+      <button className='clearBtn2'>Who We Are</button>
+      <button className='clearBtn2'>Company Values</button>
+      <button className='clearBtn2'>Benefits</button>
+      <button className='clearBtn2'>Apply</button>
+      </div>
+      <div className='bottomCol'>
+      <label>Subscribe</label>
+      <button className='clearBtn2'>information</button>
+      <p>eoklalkn alsk vna ndln gbpsocb skknbielns lbknspod lkns rlnblks slks. Tneno apbnlka</p>
+
+      <div className='inpDiv'>
+      <input className='footerInp'></input>
+      <button className='inpBtn'></button>
+      </div>
+      </div>
+      </div>
+
+      <br></br>
+
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
